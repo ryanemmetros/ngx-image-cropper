a1038081ed291bb6a9572f690eedfa676e1a527e
Ryan Schulte <ryan.schulte@emmetros.com>
2019-05-08 10:49:52 -0400

    Exporting EXIF function to be used by client apps if needed

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
 export { ImageCropperModule } from './src/image-cropper.module';
 export { OutputType, ImageCropperComponent } from './src/component/image-cropper.component';
 export { CropperPosition, ImageCroppedEvent } from './src/interfaces';
+export { resetExifOrientation } from './src/utils/exif.utils';
